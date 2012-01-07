7278cc4a901a70574b633ee342a8979e3a828e31
totor <victor.nguon@epitech.eu>
2012-01-07 15:56:52 +0100

    [Client] Implemented NetworkModule and class dependencies (PacketCommand, Player, UdpHandler)

--- client/Player.cpp
+++ client/Player.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 #include "Player.hpp"
 #include "Game.hpp"
+#include "NetworkModule.hpp"
 
 Player::Player() : Net::PacketHandler<>(4096, "\n"),
 		_name(""), _game(0)
 {
+	// NetworkModule::get().addUDPPlayer(*this);
 	std::cout << "Client connected" << std::endl;
 }
 
 Player::~Player()
 {
+	// NetworkModule::get().addUDPPlayer(*this);
 	std::cout << "Client disconnected" << std::endl;
+	// if (this->_game)
+	// 	this->_game->removePlayer(*this);
 }
 
 int			Player::handleInputPacket(Net::Packet &packet)
 {
 	static int			(Player::* const methods[])(Net::Packet&) = {
+			&Player::connection,
 			NULL,
-			&Player::etablished,
+			&Player::listGame,
 			NULL,
-			&Player::game,
-			&Player::endGame,
 			NULL,
+			&Player::connectGame,
 			&Player::player,
-			NULL
+			&Player::createGame
 	};
 	uint8_t			type;
 
@@ -40,22 +45,70 @@ void		Player::setGame(Game &game)
 	this->_game = &game;
 }
 
-int		Player::etablished(Net::Packet&)
+int		Player::connection(Net::Packet &packet)
+{
+	std::string		name;
+	Net::Packet		answer(2);
+
+	packet >> name;
+	this->_name = name;
+	answer << static_cast<uint8_t>(1);
+	answer << '\n';
+	this->handleOutputPacket(answer);
+	std::cout << "Player " << name << " connected" << std::endl;
+	return 1;
+}
+
+int		Player::listGame(Net::Packet&)
 {
 	return 1;
 }
 
-int		Player::game(Net::Packet&)
+int		Player::connectGame(Net::Packet &packet)
 {
-	return 1;
-}
-
-int		Player::endGame(Net::Packet&)
-{
-	return 1;
+	// uint16_t	id;
+	// packet << id;
+	// Game			*game = Server::get().getGame(id);
+	// if (game)
+	// {
+	// 	if (game->addPlayer(*this))
+	// 	{
+	// 		return 1;
+	// 	}
+	// 	Net::Packet		answer(4);
+	// 	packet << static_cast<uint8_t>(14);
+	// 	packet << static_cast<uint16_t>(1);
+	// 	packet << '\n';
+	// 	this->handleOutputPacket(answer);
+	// 	return 1;
+	// }
+	// Net::Packet		answer(4);
+	// packet << static_cast<uint8_t>(14);
+	// packet << static_cast<uint16_t>(2);
+	// packet << '\n';
+	// this->handleOutputPacket(answer);
+	// return 1;
 }
 
 int		Player::player(Net::Packet &)
 {
 	return 1;
+}
+
+int		Player::createGame(Net::Packet &packet)
+{
+	// uint8_t		maxPlayer;
+	// packet >> maxPlayer;
+	// Game		*game = Server::get().createGame(maxPlayer);
+	// if (game)
+	// {
+	// 	game->addPlayer(*this);
+	// 	return 1;
+	// }
+	// Net::Packet		answer(4);
+	// answer << static_cast<uint8_t>(14);
+	// answer << static_cast<uint16_t>(3);
+	// answer << '\n';
+	// this->handleOutputPacket(answer);
+	// return 1;
 }
