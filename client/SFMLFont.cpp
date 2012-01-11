fae33dc8baf3aef4657d63a793124b76c866b070
Raphael Chambily <raphael.chambily@epitech.net>
2012-01-11 12:37:29 +0100

    [Client] GUI Font fixed size bug

--- client/SFMLFont.cpp
+++ client/SFMLFont.cpp
@@ -9,16 +9,15 @@ SFMLFont::SFMLFont()
 SFMLFont::SFMLFont(std::string const &fileName, std::string const &strsize)
   : _window(RendererManager::get().getWindow())
 {
+  this->_font.LoadFromFile(fileName);
+  this->_str.SetFont(this->_font);
   if (strsize != "")
     {
       unsigned int size;
       std::istringstream buffer(strsize);
       buffer >> size;
-      this->_font.LoadFromFile(fileName, size);
+      this->_str.SetSize(size);
     }
-  else
-    this->_font.LoadFromFile(fileName);
-  this->_str.SetFont(this->_font);
 }
 
 SFMLFont::~SFMLFont()
