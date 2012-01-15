949dc3c523285e15e222c9db2e5b5967036eb591
Raphael Chambily <raphael.chambily@epitech.net>
2012-01-15 19:19:06 +0100

    [Client] changed score color

--- client/GSInGame.cpp
+++ client/GSInGame.cpp
@@ -142,6 +142,7 @@ void		GSInGame::displayScores()
       std::cout << "NameFont : " << this->_nameFonts[i] << std::endl;
       this->_nameFonts[i] = this->getFont("buttonFont");
       this->_nameFonts[i]->setText(ss.str());
+      this->_nameFonts[i]->setColor(255, 0, 0);
       this->_nameFonts[i]->setPosition((1024 / (this->_nbPlayers + 1)) * (i+1) - this->_nameFonts[i]->getWidth() / 2, 680);
       this->addGameObject(this->_nameFonts[i], "score", 20);
     }
@@ -150,6 +151,7 @@ void		GSInGame::displayScores()
     {
       this->_scoreFonts[i] = this->getFont("buttonFont");
       this->_scoreFonts[i]->setText("0000000");
+      this->_scoreFonts[i]->setColor(255, 0, 0);
       this->_scoreFonts[i]->setPosition((1024 / (this->_nbPlayers + 1)) * (i+1) - this->_scoreFonts[i]->getWidth() / 2, 720);
       this->addGameObject(this->_scoreFonts[i], "score", 20);
     }  
