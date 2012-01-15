b374ef3e074c307c0071cde5b8c01316ec4beb16
shotgun <miccerisier@gmail.com>
2012-01-15 10:34:59 +0100

    [Core] Add file

---
+++ common/Rules.cpp
@@ -0,0 +1,6 @@
+#include "Rules.hpp"
+
+void	Rules::wallTouchObject(GameObject &o1, GameObject &o2)
+{
+	o2.erase();
+}
---
+++ common/Rules.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "GameObject.hpp"
+
+class Rules
+{
+  public:
+	static void		wallTouchObject(GameObject&, GameObject&);
+
+  private:
+    Rules();
+};
